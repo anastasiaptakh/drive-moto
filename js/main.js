98ba6177b6bad179ffbcbb3fabf00900bce69c44
anastasiaptakh <sayeslife@mail.ru>
2020-12-08 17:43:14 +0300

    product card slider revision

--- js/main.js
+++ js/main.js
@@ -16,10 +16,14 @@ $(function(){
     });
 
     $('.product-item__favourite').on('click' , function() {
-        $('.product-item__favourite').toggleClass('product-item__favourite--active')
+        $(this).toggleClass('product-item__favourite--active')
     });
 
     $('.product-slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        prevArrow: '<button class="product-slider__slider-btn product-slider__slider-btnprev"><img src="images/arrow-black-left.svg" alt=""></button>' ,
+        nextArrow: '<button class="product-slider__slider-btn product-slider__slider-btnnext"><img src="images/arrow-black-right.svg" alt=""></button>',
 
     });
 });
