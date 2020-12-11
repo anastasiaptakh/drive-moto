a4b6ac0f87c433f485abe58e972240cc0770122a
anastasiaptakh <sayeslife@mail.ru>
2020-12-11 13:27:53 +0300

    banner section

--- js/main.js
+++ js/main.js
@@ -8,8 +8,8 @@ $(function(){
     $('.tab').on('click' , function(e){
         e.preventDefault();
 
-        $('.tab').removeClass('tab--active');
-        $('.tabs-content').removeClass('tabs-content--active');
+        $($(this).siblings()).removeClass('tab--active');
+        $($(this).parent().siblings().find('div')).removeClass('tabs-content--active');
 
         $(this).addClass('tab--active');
         $($(this).attr('href')).addClass('tabs-content--active');
